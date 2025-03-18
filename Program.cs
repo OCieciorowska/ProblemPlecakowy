b3f4db6c4775e7e2d2f25f5e92afd142a6c21c29
OCieciorowska <olacieciorowska@gmail.com>
2025-03-18 19:28:53 +0100

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using ProblemPlecakowy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemPlecakowy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Wprowadz seed: ");
+            int seed = int.Parse(Console.ReadLine());
+
+            Console.Write("Wprowadz liczbe przedmiotow: ");
+            int n = int.Parse(Console.ReadLine());
+
+            Console.Write("Wprowadz pojemnosc plecaka: ");
+            int capacity = int.Parse(Console.ReadLine());
+
+            Problem problem = new Problem(n, seed);
+            Console.WriteLine(problem);
+
+            Result result = problem.Solve(capacity);
+            Console.WriteLine(result);
+
+
+
+        }
+    }
+}
